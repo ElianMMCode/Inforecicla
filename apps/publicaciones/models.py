ea0740684523cc89a1ed387690c0287d947172ee
jlbf2005 <programacionjorgeb@gmail.com>
2026-03-11 22:35:22 -0500

    Modificacion Modelos de Publicaciones

--- apps/publicaciones/models.py
+++ apps/publicaciones/models.py
@@ -2,30 +2,114 @@ from django.db import models
 import uuid
 
 from apps.users.models import Usuario
+from config import constants
+from config.base_models import CreacionModificacionModel
 # Create your models here.
-class Publicaciones():
-    id = models.UUIDField(
-        primary_key=True,
-        default=uuid.uuid4,
-        editable=False
+
+
+##########################################################
+class CategoriaPublicacion(CreacionModificacionModel):
+
+    tipo = models.CharField(
+        max_length=30,
+        null=False,
+        choices=constants.TipoPublicacion,
+        default=constants.TipoPublicacion.NOTICIA,
+        blank=False
     )
-    
-    usuario_id = models.ForeignKey(
-        Usuario,
-        on_delete=models.CASCADE,
-        related_name="publicaciones"
-    )
-    
-    tipo_publicacion = models.ForeignKey(
-        TipoPublicacion,
-        on_delete=models.CASCADE,
-        related_name="Tipo"
-    )
-    
-    titulo = models.CharField(max_length=255, null= False)
-    
+
+    class Meta(CreacionModificacionModel.Meta):
+        verbose_name = "Categoria Publicacion"
+        verbose_name_plural = "Categorias de Publicaciones"
+        db_table = "CategoriaPublicacion"
+
+
+##########################################################
+class Publicacion(CreacionModificacionModel):
+
+    titulo = models.CharField(max_length=255, null=False)
+
     fecha_creacion = models.DateTimeField(auto_now_add=True)
-    
-class Comentarios():
-    pass
-    
+
+    # Relacion con la tabla CategoriaPublicacion
+    categoria = models.ForeignKey(
+        CategoriaPublicacion,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name='publicaciones'
+    )
+
+    # Relacion con la tabla de Usuarios (autor de la publicacion)
+    usuario = models.ForeignKey(
+        Usuario,
+        on_delete=models.RESTRICT,
+        related_name='publicaciones'
+    )
+
+    class Meta(CreacionModificacionModel.Meta):
+        verbose_name = "Publicacion"
+        verbose_name_plural = "Publicaciones"
+        db_table = "Publicacion"
+
+
+##########################################################
+class Comentario(CreacionModificacionModel):
+
+    # Relacion con tabla de Usuarios
+    usuario = models.ForeignKey(
+        Usuario,
+        on_delete=models.RESTRICT,
+        related_name='comentarios'
+    )
+
+    # Relacion con la Publicacion
+    publicacion = models.ForeignKey(
+        Publicacion,
+        on_delete=models.CASCADE,
+        related_name='comentarios'
+    )
+
+    tipo = models.CharField(
+        max_length=30,
+        null=False,
+        choices=constants.TipoPublicacion,
+        default=constants.TipoPublicacion.NOTICIA,
+        blank=False
+    )
+
+    texto = models.TextField()
+
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+
+    class Meta(CreacionModificacionModel.Meta):
+        verbose_name = "Comentario"
+        verbose_name_plural = "Comentarios"
+        db_table = "Comentario"
+
+
+######################################################################
+class Reaccion(CreacionModificacionModel):
+
+    valor = models.CharField(
+        max_length=30,
+        null=True,
+        choices=constants.Votos,
+    )
+
+    # Relacion entre Publicacion y Usuario
+    publicacion = models.ForeignKey(
+        Publicacion,
+        on_delete=models.CASCADE,
+        related_name='reacciones'
+    )
+
+    usuario = models.ForeignKey(
+        Usuario,
+        on_delete=models.RESTRICT,
+        related_name='reacciones'
+    )
+
+    class Meta(CreacionModificacionModel.Meta):
+        verbose_name = "Reaccion"
+        verbose_name_plural = "Reacciones"
+        db_table = "Reacciones"
