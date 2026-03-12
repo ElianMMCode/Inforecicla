525ad009b1964e0c8ec6814002b3f651ac04a312
ElianM <emelo.dev@pm.me>
2026-03-12 07:05:52 -0500

    cambios temporales movimientos

--- apps/operations/views.py
+++ apps/operations/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.shortcuts import get_object_or_404, render, redirect
 from apps.ecas.models import PuntoECA, Localidad
 from apps.users.models import Usuario
+from apps.inventory.models import Inventario
 from config import constants as cons
 from apps.core.service import UserService
 from apps.ecas.service import PuntoService
@@ -15,9 +16,26 @@ def _build_movimientos_context(punto):
     """
     Construye el contexto específico para la sección movimientos.
     """
+    materiales_inventario = list(
+        Inventario.objects.filter(punto_eca=punto).order_by("-fecha_modificacion")
+    )
     return {
         "seccion": "movimientos",
         "section_template": SECTION_TEMPLATES["movimientos"],
+        "gestor": punto.gestor_eca,
         "punto": punto,
-        # Aquí podrías agregar más datos relacionados con los movimientos
+        "unidades_medida": cons.UnidadMedida.choices,
+        "materiales_inventario": materiales_inventario,
+        "categoria_inventario": (
+            Inventario.objects.filter(punto_eca=punto)
+            .select_related("material__categoria")
+            .values_list("material__categoria__nombre", flat=True)
+            .distinct()
+        ),
+        "tipo_inventario": (
+            Inventario.objects.filter(punto_eca=punto)
+            .select_related("material__tipo")
+            .values_list("material__tipo__nombre", flat=True)
+            .distinct()
+        ),
     }
