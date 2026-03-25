0ee22b789344aa8f15333c64ee1d34e78ff5d203
ElianM <emelo.dev@pm.me>
2026-03-24 22:01:07 -0500

    feat/registro punto eca
    -Formulario de registro de gestor eca (usuario) y punto eca

---
+++ apps/users/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+# El app_name establece el namespace de forma automática
+app_name = "registro"
+
+urlpatterns = [
+    # Cambiamos el name a 'eca' para evitar 'registro:registro'
+    path("eca/", views.render_registro_eca, name="eca"),
+    path("login/", views.render_login, name="login"),
+]
+
