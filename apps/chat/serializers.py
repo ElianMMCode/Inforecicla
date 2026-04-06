7f20ea2d38f05d781725831660eeaaaeb1e466ca
ElianM <emelo.dev@pm.me>
2026-04-05 21:19:28 -0500

    fix/ajustes visuales  mensajes

--- apps/chat/serializers.py
+++ apps/chat/serializers.py
@@ -1,15 +1,20 @@
 from rest_framework import serializers
 from apps.chat.models import Chat, Mensaje
 
+
 class ChatSerializer(serializers.ModelSerializer):
+    ciudadano_nombre = serializers.CharField(
+        source="ciudadano.get_full_name", read_only=True
+    )
+
     class Meta:
         model = Chat
-        fields = ['id', 'punto', 'ciudadano', 'created_at']
-        read_only_fields = ['ciudadano']
+        fields = ["id", "punto", "ciudadano", "created_at", "ciudadano_nombre"]
+        read_only_fields = ["ciudadano"]
+
 
 class MensajeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Mensaje
-        fields = ['id', 'chat', 'remitente', 'texto', 'enviado_en', 'leido']
-        read_only_fields = ['chat', 'remitente']
-
+        fields = ["id", "chat", "remitente", "texto", "enviado_en", "leido"]
+        read_only_fields = ["chat", "remitente"]
