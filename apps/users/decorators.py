628a8f119605bbdce6f7ad73ac9e6ad41f7e8d72
ElianM <emelo.dev@pm.me>
2026-04-06 15:49:38 -0500

    fix/redirects rutas no permitidas
    -Rutas no permitidas para los usuario redirigen al inico

--- apps/users/decorators.py
+++ apps/users/decorators.py
@@ -9,16 +9,23 @@ def ciudadano_required(view_func):
     def _wrapped_view(request, *args, **kwargs):
         # Si no está autenticado, redirigimos al login
         if not request.user.is_authenticated:
-            return redirect('/login/?next=' + request.path)
+            return redirect("/login/?next=" + request.path)
         # Si no es ciudadano, lo sacamos del view (por ejemplo, a su perfil tipo)
-        if getattr(request.user, 'tipo_usuario', None) != cons.TipoUsuario.CIUDADANO:
-            messages.error(request, 'No tienes permiso para acceder a esta sección.')
+        if getattr(request.user, "tipo_usuario", None) != cons.TipoUsuario.CIUDADANO:
+            messages.error(request, "No tienes permiso para acceder a esta sección.")
             # Redirigir al panel admin o gestor ECA o un home genérico
-            if request.user.is_staff or request.user.is_superuser or getattr(request.user, 'tipo_usuario', None) == cons.TipoUsuario.ADMIN:
-                return redirect('/panel_admin/')
-            elif getattr(request.user, 'tipo_usuario', None) == cons.TipoUsuario.GESTOR_ECA:
-                return redirect('/punto-eca/')
-            return redirect('/')
+            if (
+                request.user.is_staff
+                or request.user.is_superuser
+                or getattr(request.user, "tipo_usuario", None) == cons.TipoUsuario.ADMIN
+            ):
+                return redirect("/panel_admin/")
+            elif (
+                getattr(request.user, "tipo_usuario", None)
+                == cons.TipoUsuario.GESTOR_ECA
+            ):
+                return redirect("/punto-eca/")
+            return redirect("/inicio/")
         return view_func(request, *args, **kwargs)
-    return _wrapped_view
 
+    return _wrapped_view
