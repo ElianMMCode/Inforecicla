6f5abc8e3157b83cea3ba0a8623a799aabae51e2
ElianM <emelo.dev@pm.me>
2026-04-28 13:41:38 -0500

    feat/pytest
    - Implementacion y configuracion de la libreria pytest para la
    implementación de la pruebas unitarias
    - Creacion de db ("test_inforecicla"), para la realización de las
    pruebas

--- apps/ecas/tests.py
+++ apps/ecas/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+import pytest
+from django.db import connection
 
-# Create your tests here.
+
+@pytest.mark.django_db
+def test_verificar_entorno_bd():
+    """
+    Prueba de infraestructura para garantizar el aislamiento de la BD.
+    """
+    # Extraemos el nombre de la BD directamente de la conexión activa
+    db_name = connection.settings_dict["NAME"]
+
+    # Imprimimos el nombre en la terminal para inspección visual
+    print(f"\n[AUDITORÍA DE ENTORNO] Pytest está operando en la BD: {db_name}")
+
+    # Validamos por código que la BD contenga el prefijo 'test_'
+    # o el nombre exacto que definiste en settings_test.py
+    assert "test_inforecicla" in db_name, (
+        f"¡ALERTA! Conectado a la BD incorrecta: {db_name}"
+    )
